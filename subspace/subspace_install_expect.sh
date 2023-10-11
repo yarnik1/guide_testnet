0a02233aa66d5a44ef9c8bf8b332c87e3bc80e01
WellNode <39333155+yarnik1@users.noreply.github.com>
2023-10-11 18:17:37 +0300

    Update and rename expect to subspace_install_expect.sh

--- subspace/subspace_install_expect.sh
+++ subspace/subspace_install_expect.sh
@@ -4,15 +4,17 @@
 set timeout -1
 
 # Извлечение значений из переменных окружения
-set WALLET_ADDRESS [exec env | grep WALLET_ADDRESS | cut -d= -f2]
-set SUBSPACE_NODENAME [exec env | grep SUBSPACE_NODENAME | cut -d= -f2]
+#set WALLET_ADDRESS [exec env | grep WALLET_ADDRESS | cut -d= -f2]
+#set SUBSPACE_NODENAME [exec env | grep SUBSPACE_NODENAME | cut -d= -f2]
 
 # Проверка, установлены ли переменные
+set WALLET_ADDRESS stC2C46EFLtcdvPLL74KsRtSkEPMq4LExnRTL2vtc38rPsvr3
 if { $WALLET_ADDRESS == "" } {
     puts "Ошибка: WALLET_ADDRESS не установлена"
     exit 1
 }
 
+set SUBSPACE_NODENAME WellNode
 if { $SUBSPACE_NODENAME == "" } {
     puts "Ошибка: SUBSPACE_NODENAME не установлена"
     exit 1
